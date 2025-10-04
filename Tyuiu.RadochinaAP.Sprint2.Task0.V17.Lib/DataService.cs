5a839c8a6983f90a9f5432175b4f9184668481c1
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-10-04 03:25:17 +0300

    Add project files.

---
+++ Tyuiu.RadochinaAP.Sprint2.Task0.V17.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.RadochinaAP.Sprint2.Task0.V17.Lib
+{
+    public class DataService : ISprint2Task0V17
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x - 310 == y;
+            res[1] = x + 1 != 1066;
+            res[2] = x < y;
+            res[3] = y + 10 > x;
+            res[4] = x <= y;
+            res[5] = y + 1 >= x;
+
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.RadochinaAP.Sprint2.Task0.V17/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.RadochinaAP.Sprint2.Task0.V17.Lib;
+
+namespace Tyuiu.RadochinaAP.Sprint2.Task0.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 1065;
+            int y = 755;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: Операции сравнения                                               *");
+            Console.WriteLine("* Задание #0                                                             *");
+            Console.WriteLine("* Вариант #17                                                            *");
+            Console.WriteLine("* Выполнила: Радочина Арина Павловна | ПИНб-25-1                         *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу из операций сравнений и арифметических выражений,   *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив)                   *");
+            Console.WriteLine("*                                                                        *");
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
