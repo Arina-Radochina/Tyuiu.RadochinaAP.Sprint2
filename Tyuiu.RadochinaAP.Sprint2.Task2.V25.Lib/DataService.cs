c8196992a7f1ef03b61dbaa7cc934fe6ab7b5c3a
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-10-04 17:10:18 +0300

    Task2

---
+++ Tyuiu.RadochinaAP.Sprint2.Task2.V25.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.RadochinaAP.Sprint2.Task2.V25.Lib
+{
+    public class DataService : ISprint2Task2V25
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+
+            if ((x >= 1) && (x <= 13) && (y >= 3) && (y <= 12))
+            {
+                res = true;
+            }
+            else 
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
