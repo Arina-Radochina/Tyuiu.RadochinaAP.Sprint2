bb4fdd156d3b3ae2efac1c566f037bee9f8f817b
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-10-04 21:50:03 +0300

    Task4

---
+++ Tyuiu.RadochinaAP.Sprint2.Task3.V1.Test/DataServiceTest.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.RadochinaAP.Sprint2.Task3.V1.Lib;
+
+namespace Tyuiu.RadochinaAP.Sprint2.Task3.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+
+            DataService ds = new DataService();
+            double x = 8;
+            double res = ds.Calculate(x);
+            double wait = 2884.812;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = -17.027;
+            Assert.AreEqual(wait, res);
+        }
+
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = ds.Calculate(x);
+            double wait = -10;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -6;
+            double res = ds.Calculate(x);
+            double wait = -65.995;
+            Assert.AreEqual(wait, res);
+        }
+    }
+
+}
---
+++ Tyuiu.RadochinaAP.Sprint2.Task3.V1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.RadochinaAP.Sprint2.Task3.V1.Lib;
+
+namespace Tyuiu.RadochinaAP.Sprint2.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                              *");
+            Console.WriteLine("* Тема: Вложенные операторы if-else                                                                      *");
+            Console.WriteLine("* Задание #3                                                                                             *");
+            Console.WriteLine("* Вариант #1                                                                                             *");
+            Console.WriteLine("* Выполнила: Радочина Арина Павловна | ПИНб-25-1                                                         *");
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с использованием                    *");
+            Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит значение переменной X с клавиатуры. *");
+            Console.WriteLine("*                                                                                                        *");
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                       *");
+            Console.WriteLine("**********************************************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                             *");
+            Console.WriteLine("**********************************************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey(); 
+
+
+        }
+    }
+}
