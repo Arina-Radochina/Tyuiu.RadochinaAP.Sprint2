9f36836060bc6c4d7538e2a1140ce45d93f30db9
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-10-05 16:00:46 +0300

    Task4

---
+++ Tyuiu.RadochinaAP.Sprint2.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.RadochinaAP.Sprint2.Task4.V18.Lib;
+
+namespace Tyuiu.RadochinaAP.Sprint2.Task4.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 4;
+            double res = ds.Calculate(x, y);
+            double wait = 41.5;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 8;
+            double res = ds.Calculate(x, y);
+            double wait = 83.75;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RadochinaAP.Sprint2.Task4.V18/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.RadochinaAP.Sprint2.Task4.V18.Lib;
+
+namespace Tyuiu.RadochinaAP.Sprint2.Task4.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                              *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                                               *");
+            Console.WriteLine("* Задание #4                                                                                             *");
+            Console.WriteLine("* Вариант #18                                                                                            *");
+            Console.WriteLine("* Выполнила: Радочина Арина Павловна | ПИНб-25-1                                                         *");
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с исппользованием тернарного оператора,       *");
+            Console.WriteLine("* где пользователь вводит значение переменных x, y с клавиатуры                                          *");
+            Console.WriteLine("*                                                                                                        *");
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                       *");
+            Console.WriteLine("**********************************************************************************************************");
+
+            Console.WriteLine("Введите значение переменной Х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+
+            Console.WriteLine("**********************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                             *");
+            Console.WriteLine("**********************************************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
