2b7d3584cd7f8675489099502890cd7d6052758e
Tyuiu.RadochinaAP@outlook.coom <arinaradochinatyuiu@mail.ru>
2025-10-11 15:04:36 +0300

    .

---
+++ Tyuiu.RadochinaAP.Sprint2.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.RadochinaAP.Sprint2.Task7.V15.Lib;
+namespace Tyuiu.RadochinaAP.Sprint2.Task7.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double y = 1;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
